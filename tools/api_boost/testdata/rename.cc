99c2cbe0077d10350d37696a2d896d5a6f935e53
Harvey Tuch <htuch@google.com>
2019-12-21 14:26:07 -0500

    wip
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- tools/api_boost/testdata/rename.cc
+++ tools/api_boost/testdata/rename.cc
@@ -1,8 +1,11 @@
 #include "envoy/api/v2/route/route.pb.h"
 
+using envoy::api::v2::route::RouteAction;
+using MutableRouteActionAccessor = RouteAction* (RouteAction::*)();
+
 void test() {
   envoy::api::v2::route::RouteAction route_action;
   route_action.host_rewrite();
   route_action.set_host_rewrite("blah");
-  reinterpret_cast<void>(&envoy::api::v2::route::RouteAction::set_host_rewrite);
+  MutableRouteActionAccessor foo = &envoy::api::v2::route::RouteAction::set_host_rewrite;
 }
