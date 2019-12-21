a488ebb76f7352c6df164d745dd11b9759b44dd2
Harvey Tuch <htuch@google.com>
2019-12-21 14:26:38 -0500

    wip
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- tools/api_boost/testdata/rename.cc
+++ tools/api_boost/testdata/rename.cc
@@ -7,5 +7,5 @@ void test() {
   envoy::api::v2::route::RouteAction route_action;
   route_action.host_rewrite();
   route_action.set_host_rewrite("blah");
-  MutableRouteActionAccessor foo = &envoy::api::v2::route::RouteAction::set_host_rewrite;
+  MutableRouteActionAccessor foo = &envoy::api::v2::route::RouteAction::mutable_host_rewrite;
 }
