f8265fd453f952e4cf07be224db381f51f357c0d
Harvey Tuch <htuch@google.com>
2020-01-14 21:17:37 -0500

    wip
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- test/common/upstream/host_overhead_test.cc
+++ test/common/upstream/host_overhead_test.cc
@@ -15,22 +15,26 @@ double measurePerHostOverhead(std::function<void(Cluster&)> mutate_fn) {
   Cluster cluster;
   Stats::TestUtil::MemoryTest memory_test;
   const size_t start_bytes = memory_test.consumedBytes();
-  for (uint32_t i=0; i < HostCount; ++i) {
+  for (uint32_t i = 0; i < HostCount; ++i) {
     mutate_fn(cluster);
   }
   const size_t consumed_bytes = memory_test.consumedBytes() - start_bytes;
   return (1.0 * consumed_bytes) / HostCount;
 }
 
-TEST(HostOverheadTest, All) { 
-  const double hosts_overhead = measurePerHostOverhead([](Cluster& cluster) {
-    cluster.add_hosts();
-  });
+TEST(HostOverheadTest, All) {
+  const double hosts_overhead =
+      measurePerHostOverhead([](Cluster& cluster) { cluster.add_hosts(); });
   const double load_assignment_overhead = measurePerHostOverhead([](Cluster& cluster) {
     if (cluster.load_assignment().endpoints().empty()) {
       cluster.mutable_load_assignment()->add_endpoints();
     }
-    cluster.mutable_load_assignment()->mutable_endpoints(0)->add_lb_endpoints();
+    cluster.mutable_load_assignment()
+        ->mutable_endpoints(0)
+        ->add_lb_endpoints()
+        ->mutable_endpoint()
+        ->mutable_address()
+        ->mutable_socket_address();
   });
   ENVOY_LOG_MISC(debug, "hosts overhead per host: {}", hosts_overhead);
   ENVOY_LOG_MISC(debug, "load_assignment overhead per host: {}", load_assignment_overhead);
