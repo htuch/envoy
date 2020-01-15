018293f683d4e61b642fa4c90162945b3c03359b
Harvey Tuch <htuch@google.com>
2020-01-14 21:10:14 -0500

    wip
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- test/common/upstream/host_overhead_test.cc
+++ test/common/upstream/host_overhead_test.cc
@@ -1,12 +1,20 @@
-#include "common/memory/stats.h"
+#include "test/common/stats/stat_test_utility.h"
+
+#include "envoy/config/cluster/v3alpha/cluster.pb.h"
 
 #include "gtest/gtest.h"
 
 namespace Envoy {
 namespace {
 
-TEST_F(HostOverheadTest, All) { 
+TEST(HostOverheadTest, All) { 
+  const uint32_t HostCount = 100000;
   Stats::TestUtil::MemoryTest memory_test;
+  envoy::config::cluster::v3alpha::Cluster cluster;
+  ENVOY_LOG_MISC(debug, "HTD {}", memory_test.consumedBytes());
+  for (uint32_t i=0; i < HostCount; ++i) {
+    cluster.add_hosts();
+  }
   ENVOY_LOG_MISC(debug, "HTD {}", memory_test.consumedBytes());
 }
 
