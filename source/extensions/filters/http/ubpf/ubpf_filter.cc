1984912ec3a118efd3d8f416df4c3b48693e1335
Harvey Tuch <htuch@google.com>
2021-11-12 18:33:32 +0000

    wip
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/extensions/filters/http/ubpf/ubpf_filter.cc
+++ source/extensions/filters/http/ubpf/ubpf_filter.cc
@@ -2,6 +2,16 @@
 
 #include "source/common/common/logger.h"
 
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+#include "ubpf.h"
+
+#ifdef __cplusplus
+}
+#endif
+
 namespace Envoy {
 namespace Extensions {
 namespace HttpFilters {
@@ -9,6 +19,11 @@ namespace Ubpf {
 
 Filter::Filter() {
   ENVOY_LOG_MISC(debug, "ubpf: init");
+  auto* vm = ubpf_create();
+  ENVOY_LOG_MISC(debug, "ubpf: VM created");
+
+  ubpf_destroy(vm);
+  ENVOY_LOG_MISC(debug, "ubpf: VM destroyed");
 }
 
 } // namespace Ubpf
