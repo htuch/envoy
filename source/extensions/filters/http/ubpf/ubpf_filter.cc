0fb6635c46a7ba8a921112d8b1d11688b589fe5f
Harvey Tuch <htuch@google.com>
2021-11-12 19:16:19 +0000

    wip
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- source/extensions/filters/http/ubpf/ubpf_filter.cc
+++ source/extensions/filters/http/ubpf/ubpf_filter.cc
@@ -18,13 +18,18 @@ Filter::Filter(Api::Api& api) {
   const std::string elf_contents = api.fileSystem().fileReadToEnd("hello.o");
   ENVOY_LOG_MISC(debug, "ubpf: ELF size {} bytes", elf_contents.size());
 
-  char *err = nullptr;
-  const int ret = ubpf_load_elf(vm, elf_contents.data(), elf_contents.size(), &err);
-  RELEASE_ASSERT(ret == 0, "uBPF ELF load failure");
+  {
+    char *err = nullptr;
+    const int ret = ubpf_load_elf(vm, elf_contents.data(), elf_contents.size(), &err);
+    RELEASE_ASSERT(ret == 0, "uBPF ELF load failure");
+  }
 
-  uint64_t bpf_ret_value = 0;
-  ret = ubf_exec(vm, nullptr, 0, &bpf_ret_value);
-  RELEASE_ASSERT(ret == 0, "uBPF exec failure");
+  {
+    uint64_t bpf_ret_value = 0;
+    const int ret = ubpf_exec(vm, nullptr, 0, &bpf_ret_value);
+    RELEASE_ASSERT(ret == 0, "uBPF exec failure");
+    ENVOY_LOG_MISC(debug, "ubpf: output {}", bpf_ret_value);
+  }
 
   ubpf_destroy(vm);
   ENVOY_LOG_MISC(debug, "ubpf: VM destroyed");
